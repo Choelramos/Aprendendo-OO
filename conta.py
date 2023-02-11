a084a03782b427f9a5734c3c9204739d3534cd18
Joel <joel4785ramos@gmail.com>
2023-02-11 18:48:22 -0300

    Adicionando métodos privados

--- conta.py
+++ conta.py
@@ -8,20 +8,29 @@ class Conta:
     def extrato(self):
         print("O saldo {} está na conta de {}".format(self.__saldo, self.__titular))
 
-    def saca(self, valor):
-        self.__saldo -= valor
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_para_saque = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_para_saque
+
+    def __saca(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} é maior que seu saldo!".format(self.__pode_sacar(valor)))
 
     def deposita(self, valor):
         self.__saldo += valor
 
     def transfere(self, valor, destino):
-        self.saca(valor)
+        self.__saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
     @property
