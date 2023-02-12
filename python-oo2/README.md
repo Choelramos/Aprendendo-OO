7548e47b59fde801dfb6addecef944da71d64f8f
Choel_Ramos <107724791+Choelramos@users.noreply.github.com>
2023-02-12 20:53:01 -0300

    second works at the oo

---
+++ python-oo2/README.md
@@ -0,0 +1,2 @@
+# Aprimorando_OO
+This project is about my aprimorations of the Pyhon OO
---
+++ python-oo2/modelo.py
@@ -0,0 +1,50 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporada):
+        super().__init__(nome, ano)
+        self.temporada = temporada
+
+
+vingadores = Filme("Vingadores - guerra infinita", 2019, 160)
+breakingbad = Serie("breaking bad", 2008, 5)
+
+vingadores.dar_like()
+vingadores.dar_like()
+breakingbad.dar_like()
+
+print(f' Nome: {vingadores.nome}, Ano: {vingadores.ano}, Duração: {vingadores.duracao} Likes: {vingadores.likes}')
+
+print(f'Nome: {breakingbad.nome} Ano: {breakingbad.ano}, Temporadas: {breakingbad.temporada} Likes: {breakingbad.likes}')
+
+
+
+
+
+
