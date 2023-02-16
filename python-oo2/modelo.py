ec71d8ee74b3a8a6d6766ba67113cb09b4b99524
Joel <joel4785ramos@gmail.com>
2023-02-16 09:46:28 -0300

    Adiconando playlist na composição

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -20,7 +20,8 @@ class Programa:
         self._nome = novo_nome.title()
 
     def __str__(self):
-        return f'{self._nome} - {self.ano} - {self._likes} Likes'
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'  # o '__str__' é um método doble underscore, utilizado para fazer a representação textual do objeto
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
@@ -28,7 +29,7 @@ class Filme(Programa):
         self.duracao = duracao
 
     def __str__(self):
-        return f'{self.nome} - {self.ano} - {self.duracao} min'  # o '__str__' vai retornar o valor como uma string
+        return f'{self.nome} - {self.ano} - {self.duracao} min'
 
 
 class Serie(Programa):
@@ -40,15 +41,38 @@ class Serie(Programa):
         return f'{self.nome} - {self.ano} - {self.temporada} temporadas'
 
 
+class Playlist:
+    def __int__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
+
+
 vingadores = Filme("Vingadores - guerra infinita", 2019, 160)
 breakingbad = Serie("breaking bad", 2008, 5)
+ruptura = Serie("Ruptura", 2022, 1)
+fuga = Filme("Fuga das galinhas", 1999, 100)
 
+
+fuga.dar_like()
+fuga.dar_like()
+fuga.dar_like()
+ruptura.dar_like()
+ruptura.dar_like()
+ruptura.dar_like()
+ruptura.dar_like()
+ruptura.dar_like()
 vingadores.dar_like()
 vingadores.dar_like()
 breakingbad.dar_like()
 
-filmes_e_series = [vingadores, breakingbad]
 
-for programa in filmes_e_series:
+filmes_e_series = [vingadores, breakingbad, ruptura, fuga]
+
+playlist_fim_de_semana = Playlist("fim_de_semana", filmes_e_series)
+
+for programa in playlist_fim_de_semana.programas:
     print(programa)
 
