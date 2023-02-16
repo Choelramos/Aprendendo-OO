b7fd549d73ebee46bcb91e7e173862a556c6bada
Joel <joel4785ramos@gmail.com>
2023-02-16 15:34:29 -0300

    Usando outro método para retornar lista

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -41,18 +41,26 @@ class Serie(Programa):
         return f'{self.nome} - {self.ano} - {self.temporada} temporadas'
 
 
-class Playlist(list):
+class Playlist():
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        # super().__init__(programas)  # chamando a função list e passando um parâmetro
+        self._programs = programas
 
+    @property
+    def listagem(self):
+        return self._programs
 
+    @property
+    def tamanho(self):
+        return len(self._programs)
 
 vingadores = Filme("Vingadores - guerra infinita", 2019, 160)
 breakingbad = Serie("breaking bad", 2008, 5)
 ruptura = Serie("Ruptura", 2022, 1)
 fuga = Filme("Fuga das galinhas", 1999, 100)
 
+# Ainda vamos melhorar isso, aqui estão os likes e métodos de impressão:
 
 fuga.dar_like()
 fuga.dar_like()
@@ -66,14 +74,16 @@ vingadores.dar_like()
 vingadores.dar_like()
 breakingbad.dar_like()
 
+# Colocando os objetos em listas:
 
 filmes_e_series = [vingadores, breakingbad, ruptura, fuga]
+minha_playlist = Playlist("fim_de_semana", filmes_e_series)
 
-playlist_fim_de_semana = Playlist("fim_de_semana", filmes_e_series)
+# Mostrando na tela:
 
-print(f'Tem o filmes vingadores na lista? {vingadores in playlist_fim_de_semana}')
-print(f'Quantidade de programas: {len(playlist_fim_de_semana)}')
+print(f'Tem o filme vingadores na lista? {vingadores in minha_playlist.listagem}')
+print(f'Quantidade de programas: {len(minha_playlist.listagem)}')
 
-for programa in playlist_fim_de_semana:
+for programa in minha_playlist.listagem:
     print(programa)
 
