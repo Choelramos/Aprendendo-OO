f644e197ebd21547513c6f327f318e1aa7452fb3
Joel <joel4785ramos@gmail.com>
2023-02-18 16:24:17 -0300

    Fazendo teste com função de ordem superior

---
+++ sintaxe-basica/exer_2.py
@@ -0,0 +1,18 @@
+# Criando funções de ondem superior
+
+def multiplicar_por(multiplicador):
+    def mult(multiplicando):
+        return multiplicando * multiplicador
+
+    return mult
+
+
+multiplicar_por_10 = multiplicar_por(10)
+print(multiplicar_por_10(1))
+print(multiplicar_por_10(2))
+
+
+multiplicar_por_5 = multiplicar_por(5)
+print(multiplicar_por_5(1))
+print(multiplicar_por_5(2))
+
