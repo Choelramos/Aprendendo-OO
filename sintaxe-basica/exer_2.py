ddc18e4bdda03bc4f8e00dd6ce8411e6d83a3fca
Joel <joel4785ramos@gmail.com>
2023-02-18 19:20:19 -0300

    Usando lambda na função de ordem superior

--- sintaxe-basica/exer_2.py
+++ sintaxe-basica/exer_2.py
@@ -1,16 +1,29 @@
 # Criando funções de ondem superior
 
-def multiplicar_por(multiplicador):
-    def mult(multiplicando):
-        return multiplicando * multiplicador
+# def multiplicar_por(multiplicador):
+#     def mult(multiplicando):
+#         return multiplicando * multiplicador
+#     return mult
+#
+#
+# multiplicar_por_10 = multiplicar_por(10)
+# print(multiplicar_por_10(1))
+# print(multiplicar_por_10(2))
+#
+#
+# multiplicar_por_5 = multiplicar_por(5)
+# print(multiplicar_por_5(1))
+# print(multiplicar_por_5(2))
 
-    return mult
+# Usando função anônima lambda para resolver o mesmo problema
+
+def multiplicar_por(multiplicador):
+    return lambda multiplicando: multiplicador * multiplicando
 
 
 multiplicar_por_10 = multiplicar_por(10)
 print(multiplicar_por_10(1))
 print(multiplicar_por_10(2))
-
 
 multiplicar_por_5 = multiplicar_por(5)
 print(multiplicar_por_5(1))
