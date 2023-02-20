4b18b6dbd856fd3384f4b43b4d7c88add35e845e
Joel <joel4785ramos@gmail.com>
2023-02-20 17:11:38 -0300

    Fazendo novos exercícios de programação funcional

--- sintaxe-basica/programacao_funcional.py
+++ sintaxe-basica/programacao_funcional.py
@@ -27,11 +27,18 @@
 
 # imprimindo apenas números pares:
 
-lista = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+# lista = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+#
+# pares = filter(lambda item: item % 2 == 0, lista)
+# print(list(pares))
 
-pares = filter(lambda item: item % 2 == 0, lista)
-print(list(pares))
+# -------------------------------------------------------------------------------------------------
+# Arredondando os valores da lista de números na mesma ordem da lista de precisão:
 
+lista_numeros = [9.15613, 7.84651, 3.00123, 6.5648]
+lista_precisao = [2, 2, 3, 3]
 
+arredondamento = lambda x, y: round(x, y)
 
-
+resultado = list(map(arredondamento, lista_numeros, lista_precisao))
+print(resultado)
