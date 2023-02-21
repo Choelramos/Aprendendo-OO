fd3ba3894237833390fdf02fd7025ac9195801c4
Joel <joel4785ramos@gmail.com>
2023-02-21 14:39:37 -0300

    Fazendo mais exercícios de threads

---
+++ programaca_paralela/exer_thread1.py
@@ -0,0 +1,22 @@
+from threading import Thread
+from time import sleep
+
+
+def thread(mensagem, tempo_execucao):
+    print(f'\nExecutando a {mensagem}')
+    sleep(tempo_execucao)
+    print(f'\nFinalizando a {mensagem}')
+
+
+thread1 = Thread(target=thread, args=("Thread 1", 3))
+thread2 = Thread(target=thread, args=("Thread 2", 2))
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+
+
+print("\nExecução concluída")
+
+
+
